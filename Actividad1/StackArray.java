c336009b0d12bab5cbf2df636920e66719e7375c
Alexander1909 <131818021+Alexander1909@users.noreply.github.com>
2025-05-06 16:12:16 -0500

    v4

--- Actividad1/StackArray.java
+++ Actividad1/StackArray.java
@@ -3,28 +3,26 @@ package Actividad1;
 class StackArray<E> implements Stack<E> {
     private E[] array;
     private int tope;
-    public StackArray(int n){
-    this.array = (E[])new Object[n];
-    tope = -1;
+    @SuppressWarnings("unchecked")
+    public StackArray(int tamaño) {
+        array = (E[]) new Object[tamaño];
+        tope = -1;
     }
-    public void push(E x){
-    // include here your code
-    } 
+
+    public void push(E elemento) {
+        if (isFull()) {
+            System.out.println("La pila está llena.");
+            return;
+        }
+        tope++;
+        array[tope] = elemento;
+    }
+
     public E pop() throws ExceptionIsEmpty {
-        // include here your code
+        if (isEmpty()) {
+            throw new ExceptionIsEmpty("La pila está vacía.");
+        }
+        E elemento = array[tope];
+        tope--;
+        return elemento;
     }
-    public E top() throws ExceptionIsEmpty {
-        // include here your code
-    }
-    public boolean isEmpty() {
-        return this.tope == -1;
-    }
-    public boolean isFull (){
-        // include here your code
-    }
-        //The elements must be included in the chain from the one at the top
-        //to the one at the bottom of the stack.
-    public String toString(){
-        // include here your code
-    }
-}       
